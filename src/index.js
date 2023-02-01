9e70781f2c69237ec46d1d66e1b6402b59cc5b48
Yavorskiy Maksim <maxfatal@mail.ru>
2023-02-01 21:43:07 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-
-// You should implement your task here.
+// let arr =[
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+// ];
 
 module.exports = function towelSort (matrix) {
+let newArr=[];
+let flag = true;
+if (matrix===undefined) {
   return [];
 }
+for(let i =0; i<matrix.length; i++) {
+    if(flag) {
+      for(let k=0; k<matrix[i].length; k++) {
+        newArr.push(matrix[i][k])
+        flag=false
+      }
+    }
+        else { for(let m=matrix[i].length-1; m>=0; m--){
+          // console.log(m)
+          // console.log(i)
+          newArr.push(matrix[i][m])
+          flag=true
+        }
+        }    
+  // console.log(newArr)
+}
+
+  return newArr;
+}
+
+
+// console.log(towelSort ())
